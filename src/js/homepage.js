14fab57ea5dc3caa2a2c1a67ce8035a74788025d
Patrick Schreiber <ptos382@gmail.com>
2016-02-18 21:58:07 -0600

    Updated some styling

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-
+  $('.showcase-highlight').css('height', $('.showcase').innerHeight());
 });
--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  var ajaxTest = new Ajax("google.com", {}, printTest);
-  console.log(ajaxTest);
-  ajaxTest.request();
-
-  function printTest() {
-    console.log("Hello");
-  }
-});
